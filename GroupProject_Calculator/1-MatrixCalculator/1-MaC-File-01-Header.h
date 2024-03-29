3bc44a7ee5f51cb27e7daf5353dc821e8e9173ed
Radchenko Artem <ddruming@yandex.ru>
2024-03-29 22:46:58 +0300

    Создано: 1. Меню матричного калькулятора 2. Функция ввода матрицы enterMatrix 3. Функция вывода матрицы printMatrix 4. Ффункция очистки матрицы clearMatrix

---
+++ GroupProject_Calculator/1-MatrixCalculator/1-MaC-File-01-Header.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "../0-Common/0-Co-File-01-Header.h"
+#include "1-MaC-File-02-Matrix.h"
+
+Matrix enterMatrix(unsigned short, unsigned short);
+
+void printMatrix(Matrix);
+
+void clearMatrix(Matrix);
---
+++ GroupProject_Calculator/1-MatrixCalculator/1-MaC-File-04-EnterMatrix.cpp
@@ -0,0 +1,27 @@
+﻿#include "1-MaC-File-01-Header.h"
+#include "iostream"
+
+using namespace std;
+
+/*
+ * enterMatrix - Функция создающая матрицу размером NxM и заполняющая её.
+ * Функци принимает 2 параметра - n и m, где n - число строк, а m - число столбцов.
+ * Функция возвращает структуру, содержащую матрицу размером NxM и данные о этой матрице
+ */
+Matrix enterMatrix(unsigned short n, unsigned short m)
+{
+    Matrix matrixA; // Объявляем новую матрицу
+
+    matrixA.matrix = new double*[n]; // Создаем динамический массив указателей на строки матрицы
+    matrixA.n = new unsigned short {n}; // Количество строк новой матрицы
+    matrixA.m = new unsigned short {m}; // Количество столбцов новой матрицы
+
+    for (int i = 0; i < n; i++)
+    {
+        matrixA.matrix[i] = new double[m]; // Создаём i-тую строку матрицы состоящую из m столбцов
+        cout << "| Введите " << i + 1 << "-ю строку матрицы: ";
+        for (int j = 0; j < m; j++) cin >> matrixA.matrix[i][j]; // Вводим m занчений с клавиатуры
+    }
+
+    return matrixA; // Возвращаем созданную матрицу
+}
