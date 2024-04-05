bbc60417b41085b27508b22f453863defd96113c
Radchenko Artem <ddruming@yandex.ru>
2024-04-05 15:48:08 +0300

    Сделано всё, но я ленивый

---
+++ GroupProject_Calculator/2-FractionCalculator/2-FrC-File-04-multiplication-division.cpp
@@ -0,0 +1,82 @@
+﻿#include <stdio.h>
+
+#include "2-FrC-File-01-Header.h"
+
+void Multiplication_of_fractions()
+{
+    int uppernum,undernum;
+    int denominator1,nominator1; // знаменатель и числитель для 1 дроби
+    int denominator2,nominator2; // для 2 дроби
+    printf("Введите числитель и знаменатель 1 дроби: \n");scanf_s("%d %d",&nominator1,&denominator1);
+    printf("Введите числитель и знаменатель 2 дроби: \n");scanf_s("%d %d",&nominator2,&denominator2);
+    if((denominator1)!=0 && (denominator2)!=0)
+    {
+        undernum = denominator1*denominator2;
+        uppernum = nominator1*nominator2;
+        int *result = Fraction_reduction(uppernum,undernum);
+        int flag=4;
+        if(undernum==uppernum)
+        {
+            flag=1;
+            printf(" %d / %d * %d / %d = %d\n",nominator1,denominator1,nominator2,denominator2,1);
+        }
+        if((undernum==(-1*uppernum) || uppernum==(-1*undernum)))
+        {
+            flag=2;
+            printf(" %d / %d * %d / %d = %d\n",nominator1,denominator1,nominator2,denominator2,-1);
+        }
+        if(uppernum==0)
+        {
+            flag=3;
+            printf(" %d / %d * %d / %d = %d\n",nominator1,denominator1,nominator2,denominator2,0);
+        }
+        if(flag==4)
+        {
+            printf(" %d / %d * %d / %d = %d / %d\n",nominator1,denominator1,nominator2,denominator2,result[0],result[1]);
+        }
+    }
+    else{printf("Ошибка деления на ноль!\n");}
+    getchar();
+}
+////////////////////////////
+void Division_of_fractions()
+{
+    int uppernum,undernum;
+    int denominator1,nominator1; // знаменатель и числитель для 1 дроби
+    int denominator2,nominator2; // для 2 дроби
+    printf("Введите числитель и знаменатель 1 дроби: \n");scanf_s("%d %d",&nominator1,&denominator1);
+    printf("Введите числитель и знаменатель 2 дроби: \n");scanf_s("%d %d",&nominator2,&denominator2);
+    if((denominator1)!=0 && (denominator2)!=0)
+    {
+        undernum = denominator1 * nominator2;
+        uppernum = nominator1 * denominator2;
+        int *result = Fraction_reduction(uppernum,undernum);
+        int flag=4;
+        if(undernum==uppernum)
+        {
+            flag=1;
+            printf(" %d / %d : %d / %d = %d\n",nominator1,denominator1,nominator2,denominator2,1);
+        }
+        if((undernum==(-1*uppernum) || uppernum==(-1*undernum)))
+        {
+            flag=2;
+            printf(" %d / %d : %d / %d = %d\n",nominator1,denominator1,nominator2,denominator2,-1);
+        }
+        if(uppernum==0)
+        {
+            flag=3;
+            printf(" %d / %d : %d / %d = %d\n",nominator1,denominator1,nominator2,denominator2,0);
+        }
+        if(undernum<0 && uppernum<0)
+        {
+            flag=5;
+            printf(" %d / %d : %d / %d = %d / %d\n",nominator1,denominator1,nominator2,denominator2,-1*result[0],-1*result[1]);
+        }
+        if(flag==4)
+        {
+            printf(" %d / %d : %d / %d = %d / %d\n",nominator1,denominator1,nominator2,denominator2,result[0],result[1]);
+        }
+    }
+    else{printf("Ошибка деления на ноль!\n");}
+    getchar();
+}
---
+++ GroupProject_Calculator/2-FractionCalculator/2-FrC-File-05-comparison-reduction-decimal-number.cpp
@@ -0,0 +1,86 @@
+﻿#include <stdio.h>
+
+#include "2-FrC-File-01-Header.h"
+
+void Comparing_fractions()
+{
+    double fraction1, fraction2;
+    int denominator1,nominator1; // знаменатель и числитель для 1 дроби
+    int denominator2,nominator2; // для 2 дроби
+    printf("Введите числитель и знаменатель 1 дроби: \n");scanf_s("%d %d",&nominator1,&denominator1);
+    printf("Введите числитель и знаменатель 2 дроби: \n");scanf_s("%d %d",&nominator2,&denominator2);
+    if((denominator1)!=0 && (denominator2)!=0)
+    {
+        fraction1=(nominator1*1.0/denominator1);
+        fraction2=(nominator2*1.0/denominator2);
+        if (fraction1>fraction2)
+        {
+            printf(" %d / %d > %d / %d\n",nominator1,denominator1,nominator2,denominator2);
+        }
+        if(fraction1<fraction2)
+        {
+            printf(" %d / %d < %d / %d\n",nominator1,denominator1,nominator2,denominator2);
+        }
+        if(fraction1==fraction2)
+        {
+            printf(" %d / %d = %d / %d\n",nominator1,denominator1,nominator2,denominator2);
+        }
+    }
+    else{printf("Ошибка деления на ноль!\n");}
+    getchar();
+}
+////////////////////////////////
+void Call_reduction_func()
+{
+    int denominator1,nominator1; // знаменатель и числитель для 1 дроби
+    printf("Введите числитель и знаменатель сокращаемой дроби: \n");scanf_s("%d %d",&nominator1,&denominator1);
+    if((denominator1)!=0)
+    {
+        int *result = Fraction_reduction(nominator1,denominator1);
+        int flag=4;
+        if(denominator1==nominator1)
+        {
+            flag=1;
+            printf(" %d / %d = %d\n",nominator1,denominator1,1);
+        }
+        if((denominator1==(-1*nominator1) || nominator1==(-1*denominator1)))
+        {
+            flag=2;
+            printf(" %d / %d = %d\n",nominator1,denominator1,-1);
+        }
+        if(nominator1==0)
+        {
+            flag=3;
+            printf(" %d / %d = %d\n",nominator1,denominator1,0);
+        }
+        if(denominator1<0 && nominator1<0 && flag==4)
+        {
+            flag=5;
+            printf(" %d / %d = %d / %d\n",nominator1,denominator1,-1*result[0],-1*result[1]);
+        }
+        if(flag==4)
+        {
+            printf(" %d / %d = %d / %d\n",nominator1,denominator1,result[0],result[1]);
+        }
+    }
+    else{printf("Ошибка деления на ноль!\n");}
+    getchar();
+}
+////////////////////////////////
+void Fraction_to_the_decimal_system()
+{
+    double decimal=0;
+    int denominator1,nominator1; // знаменатель и числитель для 1 дроби
+    printf("Введите числитель и знаменатель переводимой дроби: \n");scanf_s("%d %d",&nominator1,&denominator1);
+    if((denominator1)!=0)
+    {
+        if(denominator1!=nominator1)
+        {
+            decimal += (float) nominator1 / (float) denominator1;
+            printf(" %d / %d = %lf\n",nominator1,denominator1,decimal);
+        }
+        else{printf(" %d / %d = 1\n",nominator1,denominator1);}
+    }
+    else{printf("Ошибка деления на ноль!\n");}
+    getchar();
+}
